25be960873b924b2693be5715a3d655c6a328273
Hussain.H <hussainharis@gmail.com>
2024-10-10 09:14:42 -0400

    Add ShapesFactory class

--- ShapeLibrary/Rectangle.cs
+++ ShapeLibrary/Rectangle.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShapeLibrary
 {
-    public class Rectangle : IShape
+    public class Rectangle : IRectangle
     {
 
         public float X { get; }
--- ShapeLibrary/ShapesFactory.cs
+++ ShapeLibrary/ShapesFactory.cs
@@ -8,6 +8,16 @@ namespace ShapeLibrary
 {
     public class ShapesFactory
     {
+            public static ICircle CreateCircle(float x, float y, float radius, Colour colour)
+            {
+                ICircle circle = new Circle(x, y, radius, colour);
+                return circle;
+            }
 
+            public static IRectangle CreateRectangle(float x, float y, float width, float height, Colour colour)
+            {
+                IRectangle rectangle = new Rectangle(x, y, width, height, colour);
+                return rectangle;
+            }
     }
 }
