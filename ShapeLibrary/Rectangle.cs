d61674c2d80a375f01e9a2659255d3870bad3766
Hussain.H <hussainharis@gmail.com>
2024-10-10 09:22:35 -0400

    Made shape classes internal + visible to test

--- ShapeLibrary/Rectangle.cs
+++ ShapeLibrary/Rectangle.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("ShapeLibraryTests")]
+
 namespace ShapeLibrary
 {
-    public class Rectangle : IRectangle
+    internal class Rectangle : IRectangle
     {
 
         public float X { get; }
