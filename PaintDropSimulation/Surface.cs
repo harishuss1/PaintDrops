a6dca906f5a141a50b759b4de74fa787ac619333
Hussain.H <hussainharis@gmail.com>
2024-10-18 09:20:45 -0400

    Implement ISurface

---
+++ PaintDropSimulation/Surface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintDropSimulation
+{
+    internal class Surface : ISurface
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public List<IPaintDrop> Drops { get; }
+
+        public Surface(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Drops = new List<IPaintDrop>();
+        }
+
+        public void AddPaintDrop(IPaintDrop drop)
+        {
+            Drops.Add(drop);
+
+            foreach (var existingDrop in Drops)
+            {
+                if (existingDrop != drop)
+                {
+                    existingDrop.Marble(drop);
+                }
+            }
+        }
+    }
+}
+
+
+
