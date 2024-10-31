3f2555df3c5e213c2c4b347f10aa3691d8d0e9a8
Hussain.H <hussainharis@gmail.com>
2024-10-31 02:04:52 -0400

    Fix test, remove unecessary code

--- PaintDropSimulationTests/PaintDropTests.cs
+++ PaintDropSimulationTests/PaintDropTests.cs
@@ -32,27 +32,35 @@ namespace PaintDropSimulation.Tests
         }
 
         [TestMethod]
-        public void PaintDrop_Marble_ShouldRecalculateVertices()
+        public void PaintDrop_Marble_ShouldRecalculateVerticesCorrectly()
         {
             var circle1 = ShapesFactory.CreateCircle(100, 100, 50, new Colour(255, 0, 0));
             var circle2 = ShapesFactory.CreateCircle(150, 150, 40, new Colour(0, 255, 0));
             var paintDrop1 = new PaintDrop(circle1);
             var paintDrop2 = new PaintDrop(circle2);
 
+            var initialVertices = new Vector[circle1.Vertices.Length];
+            for (int i = 0; i < circle1.Vertices.Length; i++)
+            {
+                initialVertices[i] = circle1.Vertices[i];
+            }
+
             paintDrop1.Marble(paintDrop2);
 
-            Assert.AreNotEqual(circle1.Vertices, paintDrop2.Circle.Vertices);
+            for (int i = 0; i < initialVertices.Length; i++)
+            {
+                Assert.AreNotEqual(initialVertices[i], circle1.Vertices[i], $"Vertex {i} was not recalculated as expected.");
+            }
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void PaintDrop_Marble_NullOther_ShouldThrowException()
+        [ExpectedException(typeof(ArgumentException))]
+        public void PaintDrop_Marble_NullOther_ShouldThrowArgumentException()
         {
             var circle1 = ShapesFactory.CreateCircle(100, 100, 50, new Colour(255, 0, 0));
             var paintDrop1 = new PaintDrop(circle1);
 
             paintDrop1.Marble(null);
-
         }
     }
 }
